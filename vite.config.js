322caf9776c01b6d629ab4aac55c97a153b347d5
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2024-05-22 16:53:44 +0300

    fix config vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "vite-plugin-dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
